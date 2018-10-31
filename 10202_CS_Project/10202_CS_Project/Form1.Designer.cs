16dc10f97d8eba8bd108168bb27696e05a6683c6
joyma980415 <joy51744@gmail.com>
2018-10-31 22:56:33 +0800

    10202Week0

---
+++ 10202_CS_Project/10202_CS_Project/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace _10202_CS_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnWeek01 = new System.Windows.Forms.Button();
+            this.lblWeek01 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(29, 19);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(0, 12);
+            this.lblTime.TabIndex = 82;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Adobe Caslon", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(814, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 81;
+            this.button1.Text = "Exit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("王漢宗中隸書繁", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(203, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(529, 35);
+            this.label1.TabIndex = 80;
+            this.label1.Text = "102 下學期 程式設計課 C#專案";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("華康正顏楷體W5", 18F);
+            this.button4.Location = new System.Drawing.Point(740, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(147, 63);
+            this.button4.TabIndex = 78;
+            this.button4.Text = "咖啡店管理系統";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label4.Location = new System.Drawing.Point(736, 18);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 77;
+            this.label4.Text = "Week10-13";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("華康正顏楷體W5", 18F);
+            this.button3.Location = new System.Drawing.Point(498, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 63);
+            this.button3.TabIndex = 76;
+            this.button3.Text = "學生管理系統";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label3.Location = new System.Drawing.Point(494, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 20);
+            this.label3.TabIndex = 75;
+            this.label3.Text = "Week04-08";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("華康正顏楷體W5", 18F);
+            this.button2.Location = new System.Drawing.Point(256, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 63);
+            this.button2.TabIndex = 74;
+            this.button2.Text = "登入系統";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(252, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 20);
+            this.label2.TabIndex = 73;
+            this.label2.Text = "Week02";
+            // 
+            // btnWeek01
+            // 
+            this.btnWeek01.Font = new System.Drawing.Font("華康正顏楷體W5", 18F);
+            this.btnWeek01.Location = new System.Drawing.Point(14, 41);
+            this.btnWeek01.Name = "btnWeek01";
+            this.btnWeek01.Size = new System.Drawing.Size(147, 63);
+            this.btnWeek01.TabIndex = 72;
+            this.btnWeek01.Text = "CRUD";
+            this.btnWeek01.UseVisualStyleBackColor = true;
+            // 
+            // lblWeek01
+            // 
+            this.lblWeek01.AutoSize = true;
+            this.lblWeek01.Font = new System.Drawing.Font("Adobe Gothic Std B", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblWeek01.Location = new System.Drawing.Point(10, 18);
+            this.lblWeek01.Name = "lblWeek01";
+            this.lblWeek01.Size = new System.Drawing.Size(69, 20);
+            this.lblWeek01.TabIndex = 71;
+            this.lblWeek01.Text = "Week01";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnWeek01);
+            this.groupBox1.Controls.Add(this.lblWeek01);
+            this.groupBox1.Location = new System.Drawing.Point(17, 141);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(904, 125);
+            this.groupBox1.TabIndex = 83;
+            this.groupBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.NavajoWhite;
+            this.ClientSize = new System.Drawing.Size(934, 306);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "馬顥心的C#專案";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnWeek01;
+        private System.Windows.Forms.Label lblWeek01;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
